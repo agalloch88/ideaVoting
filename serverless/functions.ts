96e4ead0b5f803f88ce97a45a7803fe8baa99498
Ryan Kirsch <agalloch88@gmail.com>
2023-02-07 07:31:29 -0500

    start on createBoard function, clean up template files

--- serverless/functions.ts
+++ serverless/functions.ts
@@ -1,18 +1,5 @@
 import { AWS } from '@serverless/typescript';
 
-const corsSettings = {
-  headers: [
-    // Specify allowed headers
-    'Content-Type',
-    'X-Amz-Date',
-    'Authorization',
-    'X-Api-Key',
-    'X-Amz-Security-Token',
-    'X-Amz-User-Agent',
-  ],
-  allowCredentials: false,
-};
-
 interface Authorizer {
   name: string;
   type: string;
@@ -27,42 +14,13 @@ const authorizer: Authorizer = {
 };
 
 const functions: AWS['functions'] = {
-  getFlights: {
-    handler: 'src/functions/getFlights/index.handler',
-    events: [
-      {
-        http: {
-          method: 'get',
-          path: 'flights',
-          cors: corsSettings,
-          authorizer,
-        },
-      },
-    ],
-  },
-  bookFlight: {
-    handler: 'src/functions/bookFlight/index.handler',
+  createBoard: {
+    handler: 'src/functions/createBoard/index.handler',
     events: [
       {
         http: {
           method: 'post',
-          path: 'flights/{flightID}',
-          cors: corsSettings,
-          authorizer,
-        },
-      },
-    ],
-  },
-
-  processProductCSV: {
-    handler: 'src/functions/processProductCSV/index.handler',
-    events: [
-      {
-        s3: {
-          bucket: '${self:custom.assetBucketName}',
-          event: 's3:ObjectCreated:*',
-          rules: [{ prefix: 'products/' }, { suffix: '.csv' }],
-          existing: true,
+          path: '/boards',
         },
       },
     ],
