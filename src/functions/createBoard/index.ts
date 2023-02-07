e4f9c780b692878531be1dfb647d93bf6cfe2cc9
Ryan Kirsch <agalloch88@gmail.com>
2023-02-07 07:37:21 -0500

    parse body and validateBody

--- src/functions/createBoard/index.ts
+++ src/functions/createBoard/index.ts
@@ -1,34 +1,29 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/APIResponses';
 import Dynamo from '@libs/Dynamo';
-import { Flight, Passenger } from 'src/types/flights';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
     const body = JSON.parse(event.body!);
-    const { flightID } = event.pathParameters;
-    const { passengers } = body;
-    await bookFlight({ flightID, passengers });
+    const tableName = process.env.singleTable;
+
+    const validationError = validateBody(body);
+
+    if (validationError) {
+      return validationError;
+    }
+
     return formatJSONResponse({ body: { message: 'flight successfully booked' } });
   } catch (error) {
     return formatJSONResponse({ statusCode: 500, body: error.message });
   }
 };
 
-const bookFlight = async ({
-  flightID,
-  passengers,
-}: {
-  flightID: string;
-  passengers: Passenger[];
-}) => {
-  const flight = await Dynamo.get<Flight>({
-    tableName: process.env.singleTable,
-    pkValue: flightID,
-  });
-  await Dynamo.write({
-    data: { ...flight, passengers: [...flight.passengers, ...passengers] },
-    tableName: process.env.singleTable,
-  });
+const validateBody = (body: Record<string, any?>) => {
+  if (!body.name) {
+    return formatJSONResponse({
+      body: { message: 'name is required on body', statusCode: 400 },
+    });
+  }
   return;
 };
