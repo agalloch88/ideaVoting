a057e4ff3222b1bb2ab751851e0869219e76168f
Ryan Kirsch <agalloch88@gmail.com>
2023-02-07 07:50:04 -0500

    create BoardRecord interface

--- src/functions/createBoard/index.ts
+++ src/functions/createBoard/index.ts
@@ -2,6 +2,7 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/APIResponses';
 import Dynamo from '@libs/Dynamo';
 import { CreateBoardBody } from 'src/types/apiTypes';
+import { BoardRecord } from 'src/types/dynamo';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
@@ -16,7 +17,9 @@ export const handler = async (event: APIGatewayProxyEvent) => {
 
     const { name, description, isPublic } = body as CreateBoardBody;
 
-    const data: BoardRecord = {}
+    const data: BoardRecord = {
+      
+    }
 
     return formatJSONResponse({ body: { message: 'flight successfully booked' } });
   } catch (error) {
