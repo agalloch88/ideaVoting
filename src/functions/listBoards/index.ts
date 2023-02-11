af02a78e2336c1b73e9f7e41323e41986fde07ef
Ryan Kirsch <agalloch88@gmail.com>
2023-02-11 13:34:22 -0500

    adjust response body and clean up listBoards imports

--- src/functions/listBoards/index.ts
+++ src/functions/listBoards/index.ts
@@ -1,12 +1,9 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/APIResponses';
 import Dynamo from '@libs/Dynamo';
-import { CreateBoardBody } from 'src/types/apiTypes';
 import { BoardRecord } from 'src/types/dynamo';
-import { v4 as uuid } from 'uuid';
-import { getUserId } from '../../libs/APIGateway';
 
-export const handler = async (event: APIGatewayProxyEvent) => {
+export const handler = async () => {
   try {
     const tableName = process.env.singleTable;
     // query dynamo for all boards
@@ -20,7 +17,7 @@ export const handler = async (event: APIGatewayProxyEvent) => {
     // filter out private boards using the isPublic property
     const responseData = boards.map(({pk, sk, ...rest}) => rest).filter((board) => board.isPublic);
 
-    return formatJSONResponse({ body: { message: 'board created', id: data.id } });
+    return formatJSONResponse({ body: responseData });
   } catch (error) {
     return formatJSONResponse({ statusCode: 500, body: error.message });
   }
